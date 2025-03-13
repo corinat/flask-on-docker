49c8a86fe86130f230c38450e40038195f1cc085
Corina Tudorache <corina.tudorache@quarticle.ro>
2025-03-13 14:08:19 +0200

    replaced hardcoded va,ues with variable from.env file

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -2,24 +2,22 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 APP_FOLDER = os.getenv("APP_FOLDER", basedir)  # default to current directory
+
 class Config(object):
-    SERVER_NAME = os.getenv("SERVER_NAME", "localhost:1337")  # Tell Flask the correct public URL
+    PREFERRED_URL_SCHEME = "https" if os.getenv("FLASK_HTTPS") else "http"
+    SERVER_NAME = os.getenv("SERVER_NAME", None)  # none means no default
+    DATABASE_URL = os.getenv("DATABASE_URL", None)
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
-    DATABASE_URL = os.getenv(
-        "DATABASE_URL",
-        "postgresql+psycopg2://hello_flask:hello_flask@db:5432/hello_flask_prod",
-    )
+    if DATABASE_URL:
+        if DATABASE_URL.startswith("postgres://"):
+            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'data.db')}"  # Default to SQLite
 
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     STATIC_FOLDER = f"{APP_FOLDER}/project/static"
     MEDIA_FOLDER = f"{APP_FOLDER}/project/media"
     TEMPLATE_FOLDER = f"{APP_FOLDER}/project/templates"
-
-    SECRET_KEY = os.environ.get("SECRET_KEY", "9OLWxND4o83j4K4iuopO")
-
-
