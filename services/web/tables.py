51ffceb21da2b27b611b390b245205c84ff583d5
Corina Tudorache <corina.tudorache@quarticle.ro>
2025-03-13 11:43:30 +0200

    updated docker compose commands

---
+++ services/web/tables.py
@@ -0,0 +1,21 @@
+from flask_table import Col, LinkCol, Table
+
+
+class Results(Table):
+
+    mytable_key = Col('mytable_key', show=False)
+    id = Col('id', show=False)
+    imei = Col('IMEI')
+    name = Col('Name')
+    displayname = Col('Display Name')
+    gender = Col('Gender')
+    categ = Col('Category')
+    club = Col('Club')
+    bib = Col('BIB')
+    age = Col('Age')
+    ranking = Col('Rank')
+    time_ = Col('Time')
+
+    edit = LinkCol('Edit', 'main.edit', url_kwargs=dict(id='id'))
+    delete = LinkCol('Delete', 'main.delete', url_kwargs=dict(id='id'))
+
