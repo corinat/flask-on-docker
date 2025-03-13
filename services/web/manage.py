dff6511760d70bec6dcfa64991328486046b9777
Corina Tudorache <corina.tudorache@quarticle.ro>
2025-03-13 11:47:42 +0200

    improved manage

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,12 +1,15 @@
 import os
 
 from flask.cli import FlaskGroup
-from models import CiucasRoute, Runners, app, db
+from models import CiucasRoute, Runners
+from project.app_factory import create_app, db
 from project.helper import UserHelper
 from project.insert_json_to_postgres import InsertMockDataToPostrges
 from project.mock_data.data import dummy_data
 
+app = create_app()
 cli = FlaskGroup(app)
+
 WORKDIR = os.getenv("APP_FOLDER")
 
 
@@ -16,24 +19,20 @@ def create_db():
     db.create_all()
     db.session.commit()
 
-
 @cli.command("seed_db")
 def seed_db():
     users = UserHelper.add_dummy_user_data(dummy_data)
     db.session.add_all(users)
     db.session.commit()
 
-
 @cli.command("seed_db_route")
 def seed_db_route():
-    # Open the file and load its contents
     InsertMockDataToPostrges.insert_ciucas_data_in_postgres(
         CiucasRoute,
         f"{WORKDIR}/project/mock_data/trim_all_route.json",
     )
-    print("Finished ngesting data in table")
+    print("Finished ingesting data in table")
     db.session.commit()
-
 
 @cli.command("seed_db_runners")
 def seed_db_runners():
@@ -41,14 +40,12 @@ def seed_db_runners():
         Runners,
         f"{WORKDIR}/project/mock_data/trim_runners.json",
     )
-    print("Finished ngesting data in table")
+    print("Finished ingesting data in table")
     db.session.commit()
-
 
 @cli.command("print_db")
 def print_db():
     UserHelper.print_all_data()
-
 
 if __name__ == "__main__":
     cli()
