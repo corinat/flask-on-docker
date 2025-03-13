0f197e31b85f428abba83e52e564451cef6bc578
Corina Tudorache <corina.tudorache@quarticle.ro>
2025-03-13 14:06:24 +0200

    replacing hard coded values with variables from .env file

--- services/web/db_setup.py
+++ services/web/db_setup.py
@@ -1,17 +1,32 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+# Load environment variables from .env file
+load_dotenv()
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-DATABASE_URI = "postgresql+psycopg2://hello_flask:hello_flask@db:5432/hello_flask_prod"
+# Get DATABASE_URI from .env file
+DATABASE_URI = os.getenv("DATABASE_URL")
 
+# Ensure the variable is set, otherwise raise an error
+if not DATABASE_URI:
+    raise ValueError("DATABASE_URL environment variable is not set!")
+
+# Fix old-style PostgreSQL connection URL if needed
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URI)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+
 Base = declarative_base()
 Base.query = db_session.query_property()
 
